6e3a098f7e574185b48ac5316beb14ccc2c4d85d
alpha19976 <chenda1996@gmail.com>
2020-03-08 15:47:09 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-yum install git tmux tree -y
-yum groupinstall "Development Tools" -y
-apt install git build-essential cmake tmux tree -y
-git clone https://github.com/alpha19976/vim.git && cd vim && mv .vimrc ~ && mv -f vscode_light.vim /usr/share/vim/vim81/colors/
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+sudo yum install git tmux tree -y
+syudo yum groupinstall "Development Tools" -y
+sudo apt install git build-essential cmake tmux tree -y
+sudo git clone https://github.com/alpha19976/vim.git && cd vim && mv .vimrc ~ && mv -f vscode_light.vim /usr/share/vim/vim81/colors/
+sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
