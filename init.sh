060a5650b3aaf6fd652e6479f14496ef69f1e116
alpha19976 <chenda1996@gmail.com>
2020-06-13 23:23:13 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ sudo yum groupinstall "Development Tools" -y
 sudo apt install git build-essential cmake tmux tree -y
 sudo git clone https://github.com/alpha19976/vim.git &&  cd vim && sudo mv .vimrc ~ && sudo mv -f vscode_light.vim /usr/share/vim/vim81/colors/
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim /etc/passwd
+
+
