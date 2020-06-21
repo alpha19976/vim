85a16d062eea9da3717a523bde6bb08f2db6d7fc
alpha19976 <chenda1996@gmail.com>
2020-06-21 18:45:31 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,5 +4,6 @@ sudo apt install git build-essential cmake tmux tree mysql-server -y
 sudo git clone https://github.com/alpha19976/vim.git &&  cd vim && sudo mv .vimrc ~ && sudo mv -f vscode_light.vim /usr/share/vim/vim81/colors/
 sudo git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 #vim /etc/passwd
-
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
+echo "PasswordAuthentication yes" >> /etc/ssh/sshd_config
 
